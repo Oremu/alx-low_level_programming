4dc17c4ba43f8aff1b45c49a0fde186f179a117e
Oremu <oremuokozi@gmail.com>
2022-09-09 08:56:51 -0700

    my fifth commit (puts.c)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * return : Always 0 (success)
+ * return : Always 0 (successful)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
