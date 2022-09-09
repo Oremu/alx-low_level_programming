b65712fd0137b2796be395576fa07dabd0a8cb7c
Oremu <oremuokozi@gmail.com>
2022-09-09 09:28:08 -0700

    my fifth commit (puts.c)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: Using the main function, this program prints "Programming is like a multilingual puzzle"
- * return : 0
+ * Description: Using the main function, this program prints "Programming is like building a multilingual puzzle"
+ *
+ * Return : 0
  */
 int main(void)
 {
