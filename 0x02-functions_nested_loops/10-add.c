271f53d97fe8d228a61718762ca3f4a6c5770f92
Oremu <oremuokozi@gmail.com>
2022-09-21 17:15:23 -0700

    my eleventh commit (add.c)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @i: the first number to be added
+ * @j: The second number to be added
+ *
+ * Return: the sum of the two numbers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
