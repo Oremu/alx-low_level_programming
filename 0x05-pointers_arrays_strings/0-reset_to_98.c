1bb32c4f49ad92100292d688d12066d578f2cddf
Oremu <oremuokozi@gmail.com>
2022-09-22 22:59:25 -0700

    my first commit (rest_to_98.c)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to an int as parameter and updates the value
+ * it points to 98
+ *
+ * @n: the pointer thats's passed
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
