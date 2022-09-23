7d484ddbc74d93ffb8723b39382773ceafe23f28
Oremu <oremuokozi@gmail.com>
2022-09-23 15:06:35 -0700

    my third commit (mul)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.H"
+
+/**
+ * mul - entry point
+ * @a: character to check
+ * @b: another character to check
+ * Description: multiplies two integers
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
