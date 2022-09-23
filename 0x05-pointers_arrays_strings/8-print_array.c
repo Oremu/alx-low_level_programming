9cc723acfcb8ddacf3a6c37bf6df77f38ef6eb16
Oremu <oremuokozi@gmail.com>
2022-09-23 00:21:22 -0700

    my ninth commit (print_array.c)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - this function prints n elements of an array of integers
+ *
+ * @a: the array to be considered
+ * @n: the number of elements to be printed
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
