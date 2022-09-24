9924efc7038b1f12698540128e11a3cd0d1f3f1d
Oremu <oremuokozi@gmail.com>
2022-09-23 18:10:27 -0700

    my second commit (main.c)

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming : (\n");
+
+	i = 0;
+
+	printf("Infinite loop incoming! \\o/\n");
+
+	return (0);
+}
+
