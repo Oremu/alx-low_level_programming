2e239abcb1600a5cb171f170d062a9b8b86e9958
Oremu <oremuokozi@gmail.com>
2022-09-26 05:43:35 -0700

    extra tasks (fibonacci-1.c)

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,15 @@ int main(void)
 
 	while (i < 50)
 	{
-		if(i == 0)
+		if (i == 0)
 			printf("%ld", j);
 		else if (i == 1)
 			printf(", %ld", k);
 		else
 		{
 			k += j;
-			j = k -j;
-			printf(", %ld",k);
+			j = k - j;
+			printf(", %ld", k);
 		}
 
 		++i;
