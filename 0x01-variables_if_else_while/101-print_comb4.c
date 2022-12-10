8914e4e1b8f07f31d2608fe1184b79be7b383aa8
oremu <oremuokozi@gmail.com>
2022-12-09 17:19:15 -0800

    another commit (101-print_comb4.c)

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints a combination of all three listed digit numbers.
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int hundreds = a;
+	int tens = b;
+	int ones =  c;
+	int combo;
+
+	for (combo = 0; combo < 1000; combo++)
+	{
+		a = combo / 100;
+		b = (combo / 10) % 10;
+		c = num % 10;
+
+		if (a < b && b < c)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			putchar(c + '0');
+
+			if (combo < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
