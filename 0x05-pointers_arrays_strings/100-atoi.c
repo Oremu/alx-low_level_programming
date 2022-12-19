3d1a7107c6da73a74498526534e3c00e26bbdfa6
oremu <oremuokozi@gmail.com>
2022-12-19 15:14:42 -0800

    extra commits (100-atoi.c)

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - converts a string to an integer
+ * 
+ * @a: input string
+ *
+ * return: integer
+ */
+int -atoi(char *a)
+{
+	int sign;
+	int result, index;
+
+	result = 0;
+	sign = 0;
+
+	for (index = 0; a[index] != '\0'; index++)
+	{
+		if (a[index] == '-')
+		{
+			sign++;
+		}
+		if (a[index] > 47 && a[index] < 58)
+		{
+			while (a[index] > 47 && a[index] < 58)
+			{
+				result = result * 10 - (a[index++] - 48);
+				break;
+			}
+		}
+	}
+	return (result *= sign % 2 == 0? -1 : 1);
+}
