6bda8187d1b15efedfc73649621607112b90a6ec
oremu <oremuokozi@gmail.com>
2023-02-03 20:50:08 +0530

    new commits

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,14 +3,15 @@
 /**
  * get_endianness - check endianness
  * Return: 0 if big endian, or 1 if little endian
-*/
+ */
 int get_endianness(void)
 {
-    int num;
+	int num;
 
-    num = 1;
-    if (*(char *)&num == 1)
-        return (1);
-    else
-        return (0);
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
 }
+
