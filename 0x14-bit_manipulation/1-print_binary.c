f316c65c5ed48162ecb360e7454d24cf5ec254a9
oremu <oremuokozi@gmail.com>
2023-02-03 20:40:30 +0530

    new commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,26 +4,26 @@
 /**
  * print_binary - print binary representation of a number
  * @n: decimal number to print as binary
-*/
+ */
 void print_binary(unsigned long int n)
 {
-    unsigned long int temp;
-    int shifts;
+	unsigned long int temp;
+	int shifts;
 
-    if (n == 0)
-    {
-        printf("0");
-        return;
-    }
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
 
-    for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-        ;
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
 
-    for (; shifts >= 0; shifts--)
-    {
-        if ((n >> shifts) & 1)
-            printf("1");
-        else
-            printf("0");
-    }
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
 }
